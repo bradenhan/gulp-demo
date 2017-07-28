301ce42d3210279d41fe0ebe1d932bfa28887dce
bradenhan <bradenhan@126.com>
2017-07-28 17:39:07 +0800

    代码提交

--- README.md
+++ README.md
@@ -21,12 +21,13 @@
 # install dependencies
 npm install
 
-# serve with hot reload at localhost:8080
-npm run dev
+# start program
+gulp
 
-# build for production with minification
-npm run build
+# serve start dev localhost:8000
+gulp connectDev
 
-# build for production and view the bundle analyzer report
-npm run build --report
+# serve start dist localhost:8001
+gulp connectDist 
+
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,9 @@ var gulp = require('gulp'),
     combiner = require('stream-combiner2'),
     less = require('gulp-less'),
     htmlminify = require("gulp-html-minify"),  
-    path = require('path');
+    path = require('path'), 
+    connect = require('gulp-connect'),
+    livereload = require('gulp-livereload');
 
 // 新建代码着色与显示错误日志方法，这个方法用到了gulp-util和stream-combiner2插件
 var handleError=function(err){  
@@ -26,7 +28,8 @@ gulp.task('less', function () {
     .pipe(less({ 
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('src/css')); 
+    .pipe(gulp.dest('src/css'))
+    .pipe(livereload()); 
 });
 
 // 压缩 CSS   
@@ -36,13 +39,16 @@ gulp.task('minify-css' , function(){
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(livereload()); 
 });
 
+// 生成html
 gulp.task('build-html' , function(){
     return gulp.src("src/html/*.html")
         // .pipe(htmlminify()) 暂时不压缩html
         .pipe(gulp.dest("dist/html"))
+        .pipe(livereload()); 
 });
 
 // 新建js批量压缩任务  
@@ -63,7 +69,8 @@ gulp.task('compress', function (cb) {
 gulp.task('images', function () {
   return gulp.src('src/images/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('dist/images')) 
+    .pipe(livereload()); 
 });
  
 // 编写default任务和监听任务
@@ -83,7 +90,9 @@ gulp.task('watchjs',function(){
     });  
 });  
 
+// 监控
 gulp.task('watch', function () {
+    livereload.listen();
     //监控 html  
     gulp.watch('src/html/*.html',['build-html']); 
     //监控js  
@@ -96,9 +105,29 @@ gulp.task('watch', function () {
     gulp.watch('src/images/*.*',['images']);  
 });
 
+// 监测测试地址
+gulp.task('connectDev', function () {
+  connect.server({
+    name: 'Dev App',
+    root: ['src'],
+    port: 8000,
+    livereload: true 
+  });
+});
+
+// 监测正式地址
+gulp.task('connectDist', function () {
+  connect.server({
+    name: 'Dist App',
+    root: 'dist',
+    port: 8001,
+    livereload: true  
+  });
+});
+ 
 
 // 编写default任务和监听任务
-gulp.task('default',['watchjs' , 'less', 'images','minify-css','watch','build-html'],function(){   
+gulp.task('default',['watchjs' , 'less', 'images','minify-css','watch','build-html','connectDev','connectDist'],function(){   
    
 });  
 
